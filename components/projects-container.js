9f1cc00ec6288cedf9515726a138893209bf3386
btrx <kusumadoni77@gmail.com>
2023-01-13 10:26:58 +0700

    remove unused

--- components/projects-container.js
+++ components/projects-container.js
@@ -19,11 +19,5 @@ export default function PrContainer({ src, alt, className }) {
                 </div>
             </div>
         </section>
-        // <div className={styles.container}>
-        //     <img className={styles.image} src="https://images.unsplash.com/photo-1488628075628-e876f502d67a?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=&bg=" alt="" />
-        //     <p className={styles.title}>card title</p>
-        //     <div className={styles.overlay}></div>
-        //     <div class={styles.button}><a href="#"> BUTTON </a></div>
-        // </div>
     )
 }
